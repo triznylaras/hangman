c653b45108672313a06b76f9ef3a89d11a4fcef4
triznylaras <triznylaras@gmail.com>
2021-11-26 21:46:04 +0700

    create function to guess letters

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,14 +1,60 @@
-puts 'Hangman initialized.'
+class Hangman
 
-text_file = File.read('5desk.txt')
+  def initialize
+    @secret_word = generate_word
+    @key_clues = ''
+    @secret_word.length.times { @key_clues << '_' }
+    puts @secret_word
+    @solved_letters = []
+    @incorrect_letters = []
+    turn_order(@secret_word)
+  end
 
-words = []
-text_file.each_line do |word|
-  # puts word
-  if word.length <= 12 && word.length >= 5
-    /[[:lower:]]/.match(word[0])
-    words << word
+  def turn_order(secret_word)
+    begin
+      puts 'Please enter your guess in one letter'
+      guess = gets.chomp
+      raise "Invalid input: #{guess}" unless /[[:alpha:]]/.match(guess) && guess.length == 1
+      
+      enter_guess(guess.downcase)
+    rescue StandardError => e
+      puts e.to_s
+      retry
+    end
+  end
+
+  def enter_guess(char)
+    if @secret_word.include?(char)
+      @solved_letters << char
+      add_clue(char)
+      puts
+      puts 'Good move!'
+    else
+      @incorrect_letters << char
+      puts
+      puts "Sorry that's not included"
+    end
+  end
+
+  def add_clue(char)
+    @secret_word.split('').each_with_index do |v, i|
+      @key_clues[i] = char if v == char
+    end
+  end
+
+  # puts 'Hangman initialized.'
+
+  def generate_word
+    text_file = File.open('5desk.txt', 'r')
+    words = []
+    text_file.each_line do |word|
+      word = word.chomp
+      if word.length <= 12 &&
+        word.length >= 5
+        words << word
+      end
+    end
+    text_file.close
+    words.sample
   end
 end
-
-puts words.sample
