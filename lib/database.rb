f3708fb2508f91541362544b57d640b2d358dd43
triznylaras <triznylaras@gmail.com>
2021-11-30 22:46:04 +0700

    add function to enter file name for saved game, add new param to save, add function to load game from saved file

--- lib/database.rb
+++ lib/database.rb
@@ -1,7 +1,10 @@
 module Database
   def save_game
     Dir.mkdir 'output' unless Dir.exist? 'output'
-    @filename = "#{random_name}_game.yaml"
+    puts 'Enter name for the saved game, or blank to create random name: '
+    file_name = gets.chomp
+    file_name = random_name if file_name == ''
+    @filename = "#{file_name}_game.yaml"
     File.open("output/#{@filename}", 'w') { |file| file.write save_to_yaml }
     puts "Your game has been saved with name #{@filename}"
   end
@@ -10,7 +13,9 @@ module Database
     YAML.dump(
       'secret_word' => @secret_word,
       'correct_letters' => @correct_letters,
-      'incorrect_letters' => @incorrect_letters
+      'incorrect_letters' => @incorrect_letters,
+      'key_clues' => @key_clues,
+      'remaining_guess' => @remaining_guess
     )
   end
 
@@ -37,11 +42,22 @@ module Database
 
   def load_game
     find_saved_file
+    load_saved_file
+    play_game
   end
 
   def find_saved_file
     show_file_list
     file_number = user_input(display_saved_prompt, /\d+|^exit$/)
     @saved_game = file_list[file_number.to_i - 1] unless file_number == 'exit'
+  end
+
+  def load_saved_file
+    file = YAML.safe_load(File.read("output/#{@saved_game}"))
+    @secret_word = file['secret_word']
+    @correct_letters = file['correct_letters']
+    @incorrect_letters = file['incorrect_letters']
+    @key_clues = file['key_clues']
+    @remaining_guess = file['remaining_guess']
   end
 end
